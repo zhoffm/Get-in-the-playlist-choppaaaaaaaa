233f2c46faaf6c3110a9b03444c8f1b58bfc05b2
Zach Hoffman <zhoffm@gmail.com>
2020-06-20 16:48:53 -0500

    added user id

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# TODO: 
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import logging
@@ -5,23 +7,30 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 username = 'zhoffm'
+user_id = 121776799
 scope = "user-library-read,playlist-modify-private,playlist-modify-public"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=username))
 
 
 def make_playlist():
+    # sp.user_playlist_create(username, )
+    pass
+
+def add_songs_to_playlist():
     pass
 
 def get_100_songs():
     pass
 
 
-results = sp.current_user_saved_tracks()
+results = sp.current_user_saved_tracks(limit=50)
 tracks = results.get("items")
+print(tracks)
+print(sp.next(results))
 
-while results.get("next"):
-    results = sp.next(results)
-    logging.info(results.get("items"))
-    tracks.extend(results.get("items"))
+# while results.get("next"):
+#     results = sp.next(results)
+#     logging.info(results.get("items"))
+#     tracks.extend(results.get("items"))
 
 print(len(tracks))
