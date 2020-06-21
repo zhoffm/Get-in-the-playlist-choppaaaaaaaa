701fdc5ce8d4af9a3b1d4ad84180e23dbe52d297
Zach <zhoffm@gmail.com>
2020-06-20 22:59:27 -0500

    random rambling function additions with no real point. I should probably actually draw out the architecture for this

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import logging
+import arrow
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level='WARNING')
 
 username = 'zhoffm'
 user_id = 121776799
@@ -12,25 +13,51 @@ scope = "user-library-read,playlist-modify-private,playlist-modify-public"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=username))
 
 
-def make_playlist():
-    # sp.user_playlist_create(username, )
-    pass
-
-def add_songs_to_playlist():
-    pass
-
-def get_100_songs():
-    pass
+def create_playlist(playlist_name, description=''):
+    new_playlist = sp.user_playlist_create(user_id, playlist_name, public=False, description=description)
+    if new_playlist.get('id'):
+        return new_playlist.get('id')
+    return new_playlist
 
 
-results = sp.current_user_saved_tracks(limit=50)
-tracks = results.get("items")
-print(tracks)
-print(sp.next(results))
+def add_songs_to_playlist(playlist_id, tracklist):
+    track_id_list = strip_ids_from_tracklist(tracklist)
+    sp.user_playlist_add_tracks(playlist_id, track_id_list)
 
-# while results.get("next"):
-#     results = sp.next(results)
-#     logging.info(results.get("items"))
-#     tracks.extend(results.get("items"))
 
-print(len(tracks))
+def get_100_tracks(offset=0):
+    results = sp.current_user_saved_tracks(limit=50, offset=offset)
+    tracks = results.get("items")
+
+    while len(tracks) % 100 != 0:
+        results = sp.next(results)
+        logging.info(results.get("items"))
+        tracks.extend(results.get("items"))
+
+    return results.get('next'), tracks
+
+
+def strip_ids_from_tracklist(results):
+    results_ids = [result.get('track').get('id') for result in results if results.get('track')]
+    return results_ids
+
+
+def get_all_tracks():
+    results = sp.current_user_saved_tracks(limit=50)
+    tracks = results.get("items")
+
+    while results.get("next"):
+        results = sp.next(results)
+        logging.info(results.get("items"))
+        tracks.extend(results.get("items"))
+
+    return tracks
+
+
+if __name__ == '__main__':
+    # created_playlist = create_playlist('test_playlist')
+    # print(created_playlist)
+    offset = 0
+    next_batch, tracklist = get_100_tracks()
+
+
